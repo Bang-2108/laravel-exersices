89ff32eccef9b068116c34c858317b17656a50a1
Bang-2108 <zoanthibang@gmail.com>
2025-02-25 20:28:22 +0700

    mock api

--- my-laravel/resources/views/products/index.blade.php
+++ my-laravel/resources/views/products/index.blade.php
@@ -48,28 +48,3 @@
 </body>
 
 </html>
-View: Form Thêm Sản Phẩm (resources/views/products/create.blade.php) @extends('layouts.app') @section('content')
-<div class="container mt-5">
-    <h2>Thêm sản phẩm</h2>
-    <form action="{{ route('products.store') }}" method="POST">
-        @csrf
-        <input type="text" name="name" placeholder="Tên sản phẩm" required class="form-control mb-3">
-        <textarea name="description" placeholder="Mô tả" class="form-control mb-3"></textarea>
-        <input type="number" name="price" placeholder="Giá" required class="form-control mb-3">
-        <input type="number" name="quantity" placeholder="Số lượng" required class="form-control mb-3">
-        <button type="submit" class="btn btn-success">Lưu</button>
-    </form>
-</div>
-@endsection View: Form Chỉnh Sửa Sản Phẩm (resources/views/products/edit.blade.php) @extends('layouts.app') @section('content')
-<div class="container mt-5">
-    <h2>Chỉnh sửa sản phẩm</h2>
-    <form action="{{ route('products.update', $product['id']) }}" method="POST">
-        @csrf @method('PUT')
-        <input type="text" name="name" value="{{ $product['name'] }}" required class="form-control mb-3">
-        <textarea name="description" class="form-control mb-3">{{ $product['description'] }}</textarea>
-        <input type="number" name="price" value="{{ $product['price'] }}" required class="form-control mb-3">
-        <input type="number" name="quantity" value="{{ $product['quantity'] }}" required class="form-control mb-3">
-        <button type="submit" class="btn btn-primary">Cập nhật</button>
-    </form>
-</div>
-@endsection
