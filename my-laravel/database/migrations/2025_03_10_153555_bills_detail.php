c4fd5b52ad8d1f50034e93fc2686ab2422206e87
Bang-2108 <zoanthibang@gmail.com>
2025-03-10 22:52:07 +0700

    relationship

---
+++ my-laravel/database/migrations/2025_03_10_153555_bills_detail.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_bill')->constrained('bills')->onDelete('cascade');
+            $table->foreignId('id_product')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->float('unit_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills_detail');
+    }
+};
